2d9e25a2d34b33d86b351e29d509ba10959ee379
Niklas Hanft <hanft@newsaktuell.de>
2018-07-22 12:55:50 +0200

    Remove dist from git repo

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var newsaktuell_editor_1 = require("./newsaktuell/editor/newsaktuell-editor");
-exports.default = newsaktuell_editor_1.default;
-//# sourceMappingURL=index.js.map
--- dist/newsaktuell/transformation/transformation.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=transformation.js.map
